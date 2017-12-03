eea818f2ddd58dd5ee522e2eb6068732ebe2baa6
Benjamin Sisson <benmsisson@gmail.com>
2017-12-03 14:04:21 -0500

    Added ending emergency from the map and confirmation for both ending emergencies.

--- client/src/pages/map/map.ts
+++ client/src/pages/map/map.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, AlertController} from 'ionic-angular';
+import {HttpClient} from "@angular/common/http";
 import L from "leaflet";
 
 
@@ -9,32 +10,117 @@ import L from "leaflet";
 })
 export class MapPage {
   private loaded: boolean;
+  private showMap: boolean = false;
   private map: L.Map;
   private center: L.PointTuple;
-  constructor(public navCtrl: NavController) {
+  private mapWidth: string = "500px";
+  private mapHeight: string = "500px";
+  private zero: string = "0px";
+  private uuid: string;
+  private viewHeight: number = 15;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public http: HttpClient) {
 
   }
 
+  public viewEmergency(): void {
+    this.http.get("http://localhost:8100/fetch/?&emergencyID=" + this.uuid, {"responseType": "json"}).subscribe(
+      data => {
+        this.loadMap(data["address"], data["latLng"]);
+      },
+      err => {
+        console.log(err);
+        this.showError(err.status, err.statusText);
+      }
+    );
+  }
 
-  ionViewDidEnter() {
-    if (this.loaded) {
-      return;
+  public endEmergency(): void {
+    let dialogue = this.alertCtrl.create({
+      title: 'Please confirm ending this emergency.',
+      message: 'Emergency caller will be notified.',
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Yes, End Emergency',
+          handler: () => {
+            this.sendEndHttp();
+          }
+        },
+      ],
+      cssClass: 'big-alert'
+    });
+    dialogue.present();
+  }
+
+  private sendEndHttp(): void {
+    this.http.get("http://localhost:8100/end/?&emergencyID=" + this.uuid, {"responseType": "text"}).subscribe(
+      data => {
+        this.goBack();
+      },
+      err => {
+        console.log(err);
+        this.showError(err.status, err.statusText);
+      }
+    );
+  }
+
+
+  public loadMap(address: string, latLngRaw: string) {
+    if (!this.loaded) {
+      this.initMap();
     }
-    this.center = [48.775556, 9.182778]; //Stuttgart
+    this.showMap = true;
 
-    //setup leaflet map
-    this.initMap();
+    const mapBox = document.getElementById("map");
+    mapBox.style.width = this.mapWidth;
+    mapBox.style.height = this.mapHeight;
+
+    const s = latLngRaw.split(",");
+    const latLng = new L.LatLng(Number(s[0]),Number(s[1]));
+    this.map.setView(latLng, this.viewHeight);
+
+    var marker = L.marker(latLng).addTo(this.map);
+    marker.bindPopup(address).openPopup();
   }
+
+  public goBack(): void {
+    this.uuid = "";
+    this.showMap = false;
+
+    const mapBox = document.getElementById("map");
+    mapBox.style.width = this.zero;
+    mapBox.style.height = this.zero;
+  }
+
 
   initMap() {
+    this.loaded = true;
     this.map = L.map('map', {
-      center: this.center,
       zoom: 13
     });
-
     //Add OSM Layer
     L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
       .addTo(this.map);
+  }
+
+  public showError(code: string, text: string): void {
+    let dialogue = this.alertCtrl.create({
+      title: 'Error ' + code,
+      message: 'An error occurred:\n' + text,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.goBack();
+          }
+        },
+      ],
+      cssClass: 'big-alert'
+    });
+    dialogue.present();
   }
 
 }
--- client/src/pages/wait/wait.ts
+++ client/src/pages/wait/wait.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { NavController } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
-import { Geolocation } from "@ionic-native/geolocation";
+import {NavController, AlertController} from 'ionic-angular';
+import {Geolocation} from "@ionic-native/geolocation";
 import {UniqueDeviceID} from "@ionic-native/unique-device-id";
-import { SMS } from '@ionic-native/sms';
+import {SMS} from '@ionic-native/sms';
 
-export enum EmergencyState {CALL,SEND,WAIT};
+export enum EmergencyState {CALL, SEND, WAIT};
 
 @Component({
   selector: 'page-wait',
@@ -16,10 +15,11 @@ export enum EmergencyState {CALL,SEND,WAIT};
 export class WaitPage {
   // tslint:disable-next-line: no-unused-variable
   private emergencyEnum = EmergencyState;
-  private state : EmergencyState;
+  private state: EmergencyState;
   private address: string;
   private zipcode: string;
   private isSMS: boolean;
+
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public geolocation: Geolocation, public http: HttpClient, public  uniqueDeviceID: UniqueDeviceID, public sms: SMS) {
     this.state = EmergencyState.CALL;
   }
@@ -32,7 +32,7 @@ export class WaitPage {
         {
           text: 'Cancel',
           handler: () => {
-            this.emergencyOver()
+            this.endEmergency()
           }
         },
         {
@@ -81,72 +81,93 @@ export class WaitPage {
       console.log("Error getting location");
       return;
     }
-    var uuid;
+    var deviceID;
     try {
-      uuid = await this.uniqueDeviceID.get();
+      deviceID = await this.uniqueDeviceID.get();
     } catch (e) {
-      uuid = "computer-id";
+      deviceID = "computer-id";
     }
     const latLng = location.coords.latitude + "," + location.coords.longitude;
 
     if (this.isSMS) {
-      this.sms.send("6178299064","latlng\n"+uuid+"\n"+latLng,{replaceLineBreaks:true});
+      this.sms.send("6178299064", "latlng\n" + deviceID + "\n" + latLng, {replaceLineBreaks: true});
     } else {
-      this.http.get("http://localhost:8100/latlng/?&UUID=" + uuid + "&LatLng=" + latLng, {"responseType": "text"}).subscribe(
+      this.http.get("http://localhost:8100/latlng/?&deviceID=" + deviceID + "&LatLng=" + latLng, {"responseType": "text"}).subscribe(
         data => {
           return console.log(data);
         },
         err => {
           console.log(err);
-          this.showError(err.status,err.statusText);
+          this.showError(err.status, err.statusText);
         }
       );
     }
   }
 
   public async sendAddress() {
-    var uuid;
+    var deviceID;
     try {
-      uuid = await this.uniqueDeviceID.get();
+      deviceID = await this.uniqueDeviceID.get();
     } catch (e) {
-      uuid = "computer-id";
+      deviceID = "computer-id";
     }
 
     if (this.isSMS) {
-      this.sms.send("6178299064","address\n"+uuid+"\n"+this.address+"\n"+this.zipcode,{replaceLineBreaks:true});
+      this.sms.send("6178299064", "address\n" + deviceID + "\n" + this.address + "\n" + this.zipcode, {replaceLineBreaks: true});
     } else {
       console.log("HELLO");
-      this.http.get("http://localhost:8100/address/?&UUID=" + uuid + "&Address=" + this.address + "&Zipcode=" + this.zipcode, {"responseType": "text"}).subscribe(
+      this.http.get("http://localhost:8100/address/?&deviceID=" + deviceID + "&Address=" + this.address + "&Zipcode=" + this.zipcode, {"responseType": "text"}).subscribe(
         data => {
           return console.log(data);
         },
         err => {
           console.log(err);
-          this.showError(err.status,err.statusText);
+          this.showError(err.status, err.statusText);
         }
       );
     }
     this.state = EmergencyState.WAIT;
   }
 
-  public async emergencyOver() {
-    var uuid;
+  public endEmergency(): void {
+    let dialogue = this.alertCtrl.create({
+      title: 'Please confirm ending this emergency.',
+      message: 'Emergency dispatcher will be notified.',
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Yes, End Emergency',
+          handler: () => {
+            this.sendEndHttp();
+          }
+        },
+      ],
+      cssClass: 'big-alert'
+    });
+    dialogue.present();
+  }
+
+
+  public async sendEndHttp() {
+    var deviceID;
     try {
-      uuid = await this.uniqueDeviceID.get();
+      deviceID = await this.uniqueDeviceID.get();
     } catch (e) {
-      uuid = "computer-id";
+      deviceID = "computer-id";
     }
 
     if (this.isSMS) {
-      this.sms.send("6178299064","end\n"+uuid,{replaceLineBreaks:true});
+      this.sms.send("6178299064", "end\n" + deviceID, {replaceLineBreaks: true});
     } else {
-      this.http.get("http://localhost:8100/end/?&UUID=" + uuid, {"responseType": "text"}).subscribe(
+      this.http.get("http://localhost:8100/end/?&deviceID=" + deviceID, {"responseType": "text"}).subscribe(
         data => {
           return console.log(data);
         },
         err => {
           console.log(err);
-          // No need to alert the user. We are manually ending it.
+          this.showError(err.status, err.statusText);
         }
       );
     }
@@ -154,7 +175,9 @@ export class WaitPage {
     this.navCtrl.parent.select(0);
   }
 
-  public showError(code:string, text:string): void {
+
+
+  public showError(code: string, text: string): void {
     let dialogue = this.alertCtrl.create({
       title: 'Error ' + code,
       message: 'An error occurred:\n' + text,
