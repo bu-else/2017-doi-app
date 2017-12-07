38f5717b27e054fb7f1dc8554e30181f06c94005
Michael Hendrick <MichaelHendrick@dhcp-wifi-8021x-155-41-127-41.bu.edu>
2017-12-07 15:32:35 -0500

    Updated UI/With Login Flow
    Fixed everything? Maybe made everything broken?

--- client/src/pages/main/main.ts
+++ client/src/pages/main/main.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController,NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 // import * as Config from './config.js';
 import * as $ from 'jquery';
+import {LoginPage} from "../logIn/login";
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-main',
   templateUrl: 'main.html'
 })
 export class MainPage {
-
-  constructor(public navCtrl: NavController, public storage: Storage) {
+  @ViewChild('myNav') nav: NavController;
+  constructor(public navCtrl: NavController, public storage: Storage,private alertCtrl: AlertController) {
 
   }
 
@@ -18,7 +21,18 @@ export class MainPage {
     this.storage.clear();
     // console.log(Config.get("projectName"));
   }
-
+  public goToLogin(): void{
+    this.navCtrl.setRoot(LoginPage);
+  }
+  public presentAlert(): void {
+    let alert = this.alertCtrl.create({
+      title: 'You have been logged out!',
+      subTitle: "You're all set",
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+/*
   logOut() {
     $("#form3Sbm").click(function () {
       //submitHandlerPatient("<%= configUrl %>");
@@ -42,4 +56,5 @@ export class MainPage {
       });
     })
   }
+  */
 }
